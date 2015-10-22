cc38c6fdaced4528836989d922b9558c3077004d
Jeff Russ <jeffreylynnruss@gmail.com>
2015-10-22 00:45:32 +0000

    generate pages_controller and 12 views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,37 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def grids
+  end
+
+  def buttons
+  end
+
+  def glyphs
+  end
+
+  def forms
+  end
+
+  def dropdowns
+  end
+
+  def tabs
+  end
+
+  def navbars
+  end
+
+  def list_groups
+  end
+
+  def panels
+  end
+
+  def model_windows
+  end
+
+  def collapse_carousels
+  end
+end
