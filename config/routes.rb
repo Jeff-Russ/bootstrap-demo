1e5bb7fabd64495e93d838a8f23dfb397c56b9b1
Jeff Russ <jeffreylynnruss@gmail.com>
2015-10-27 04:06:17 +0000

    Prepared for new Odds and Ends page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'pages/panels'
   get 'pages/carousels'
   get 'pages/modal_windows'
+  get 'pages/odds_n_ends'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
