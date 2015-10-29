c71b38123280942cb676275c3ae79aa276f88123
Jeff Russ <jeffreylynnruss@gmail.com>
2015-10-29 18:38:30 +0000

    model-menu is mostly done. TODO: get rid of all white and fix position of close button

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get 'pages/carousels'
   get 'pages/modal_windows'
   get 'pages/test_page'
+  get 'pages/test_page2'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
